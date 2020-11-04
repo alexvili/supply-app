18f1757d1ed4e2a49d99efc8bdfa59f92f209599
Alex Vili <al3xvili@gmail.com>
2020-11-04 18:40:59 +0200

    Bug fix

--- src/components/StocksChart.js
+++ src/components/StocksChart.js
@@ -1,48 +1,12 @@
-import React, { PureComponent, createRef } from "react";
+import React, { Component, createRef } from "react";
 import _ from "underscore";
 import Chart from "react-apexcharts";
 
-class StocksChart extends PureComponent {
+class StocksChart extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      series: this.props.series,
-      options: {
-        chart: {
-          height: 350,
-          type: "area",
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          curve: "smooth",
-        },
-        xaxis: {
-          type: "datetime",
-          categories: [
-            "2020-11-01T00:00:00.000Z",
-            "2020-11-01T01:00:00.000Z",
-            "2020-11-01T02:00:00.000Z",
-            "2020-11-01T03:00:00.000Z",
-            "2020-11-01T04:00:00.000Z",
-            "2020-11-01T05:00:00.000Z",
-            "2020-11-01T06:00:00.000Z",
-            "2020-11-01T07:00:00.000Z",
-            "2020-11-01T08:00:00.000Z",
-            "2020-11-01T09:00:00.000Z",
-            "2020-11-01T10:00:00.000Z",
-            "2020-11-01T11:00:00.000Z",
-            "2020-11-01T12:00:00.000Z",
-          ],
-        },
-        tooltip: {
-          x: {
-            format: "dd/MM/yy HH:mm",
-          },
-        },
-      },
       width: 1100,
       height: 400,
     };
@@ -50,17 +14,47 @@ class StocksChart extends PureComponent {
     this.container = createRef();
   }
 
+  options = {
+    chart: {
+      height: 350,
+      type: "area",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "2020-11-01T00:00:00.000Z",
+        "2020-11-01T01:00:00.000Z",
+        "2020-11-01T02:00:00.000Z",
+        "2020-11-01T03:00:00.000Z",
+        "2020-11-01T04:00:00.000Z",
+        "2020-11-01T05:00:00.000Z",
+        "2020-11-01T06:00:00.000Z",
+        "2020-11-01T07:00:00.000Z",
+        "2020-11-01T08:00:00.000Z",
+        "2020-11-01T09:00:00.000Z",
+        "2020-11-01T10:00:00.000Z",
+        "2020-11-01T11:00:00.000Z",
+        "2020-11-01T12:00:00.000Z",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
+      },
+    },
+  };
+
   componentDidMount() {
     this.setState({
       width: this.container.current.offsetWidth - 50,
     });
     window.addEventListener("resize", this.updateChartSizeThrottled);
-  }
-
-  componentDidUpdate() {
-    this.setState({
-      series: this.props.series,
-    });
   }
 
   componentWillUnmount() {
@@ -84,8 +78,8 @@ class StocksChart extends PureComponent {
           <div className="row">
             <div className="mixed-chart">
               <Chart
-                options={this.state.options}
-                series={this.state.series}
+                options={this.options}
+                series={this.props.series}
                 type="area"
                 width={this.state.width}
                 height={this.state.height}
